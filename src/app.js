5c78ac249a80988a027d0d630273031c7b702722
Keenan B <>
2020-02-11 06:43:35 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import AppRouter from './routers/AppRouter.js'
+import { startSetExpenses } from '../src/actions/expenses';
+import { setTextFilter } from '../src/actions/filters';
+import getVisibleExpenses from '../src/selectors/expenses'
+import configureStore from '../src/store/configureStore.js'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css'
+import './firebase/firebase'
+
+const store = configureStore();
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"))
+
+store.dispatch(startSetExpenses()).then(() => {
+    
+})
+
+ReactDOM.render(jsx, document.getElementById("app"))
